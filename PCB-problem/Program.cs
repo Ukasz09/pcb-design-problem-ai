c46afe70ce301f07538f4d063c2d2d486fb6feab
ukasz09 <246703@student.pwr.edu.pl>
2021-03-20 15:12:18 +0100

    Add tournament size investigation

--- PCB-problem/Program.cs
+++ PCB-problem/Program.cs
@@ -14,7 +14,7 @@ namespace PCB_problem
         private const string ContentHeaderText = "epochNo;bestPenalty;avgPenalty;worstPenalty;avgExecTimeMs";
 
         private static Pcb pcb;
-        private const int _defaultPopulationSize = 1000;
+        private const int _defaultPopulationSize = 250;
         private const int _defaultW1 = 40;
         private const int _defaultW2 = 1;
         private const int _defaultW3 = 2;
@@ -28,9 +28,12 @@ namespace PCB_problem
         private static void Main(string[] args)
         {
             pcb = ReadPcbData();
-            // InvestigateAffectOfPopulationSize(new[] {10, 50, 100, 500, 1000, 2000});
-            // InvestigateEpochsQty(new[] {10, 50, 100, 500, 1000, 2000});
+            InvestigateAffectOfPopulationSize(new[] {10, 50, 100, 500, 1000, 2000});
+            InvestigateEpochsQty(new[] {10, 50, 100, 500, 1000, 2000});
             InvestigateCrossoverProbability(new[] {0.1, 0.25, 0.5, 0.75, 0.9});
+            InvestigateMutationProbability(new[] {0.1, 0.25, 0.5, 0.75, 0.9});
+            InvestigateTournamentSize(new[] {0.002, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 0.9});
+            
         }
 
         private static Pcb ReadPcbData()
@@ -136,6 +139,72 @@ namespace PCB_problem
                     mutationOperator, epochsQty, startedPopulation);
                 _logger.Info(
                     $"Calulated for:  {populationSize.ToString()},{w1.ToString()},{w2.ToString()},{w3.ToString()},{w4.ToString()},{w5.ToString()},{tournamentSizePercent.ToString()},{probability.ToString()},{mutationProbability.ToString()},{epochsQty.ToString()}");
+            }
+        }
+
+        private static void InvestigateMutationProbability(IEnumerable<double> mutationProbabilities)
+        {
+            var outputFilePathPrefix = $"../../../../{ResultsDirectoryName}/mutation-probability-investigation";
+            const string outputFilePathExtension = ".csv";
+
+            var (w1, w2, w3, w4, w5) = (_defaultW1, _defaultW2, _defaultW3, _defaultW4, _defaultW5);
+            const double tournamentSizePercent = _defaultTournamentSizePercent;
+            const int populationSize = _defaultPopulationSize;
+            const int epochsQty = _defaultEpochsQty;
+            const double crossoverProbability = _defaultCrossoverProbability;
+
+            var geneticAlgorithm = new GeneticAlgorithm(pcb, w1, w2, w3, w4, w5);
+            var selectionOperator = new TournamentSelection(pcb, tournamentSizePercent, w1, w2, w3, w4, w5);
+            var crossoverOperator = new UniformCrossover(crossoverProbability);
+
+            _logger.Info("----------------------------------------------");
+            _logger.Info("-- Started mutation probability investigations --");
+            _logger.Info("----------------------------------------------");
+            var startedPopulation = GeneticAlgorithmUtils.GetStartedPopulation(pcb, populationSize);
+
+            foreach (var probability in mutationProbabilities)
+            {
+                var outputFilePath = $"{outputFilePathPrefix}-{probability.ToString()}{outputFilePathExtension}";
+                File.WriteAllLines(outputFilePath, new[] {ContentHeaderText});
+
+                var mutationOperator = new MutationA(probability);
+                GeneticAlgorithmSolution(outputFilePath, selectionOperator, geneticAlgorithm, crossoverOperator,
+                    mutationOperator, epochsQty, startedPopulation);
+                _logger.Info(
+                    $"Calulated for:  {populationSize.ToString()},{w1.ToString()},{w2.ToString()},{w3.ToString()},{w4.ToString()},{w5.ToString()},{tournamentSizePercent.ToString()},{crossoverProbability.ToString()},{probability.ToString()},{epochsQty.ToString()}");
+            }
+        } 
+        
+        private static void InvestigateTournamentSize(IEnumerable<double> tournamentSizes)
+        {
+            var outputFilePathPrefix = $"../../../../{ResultsDirectoryName}/tournament-size-investigation";
+            const string outputFilePathExtension = ".csv";
+
+            var (w1, w2, w3, w4, w5) = (_defaultW1, _defaultW2, _defaultW3, _defaultW4, _defaultW5);
+            const int populationSize = _defaultPopulationSize;
+            const int epochsQty = _defaultEpochsQty;
+            const double crossoverProbability = _defaultCrossoverProbability;
+            const double mutationProbability = _defaultMutationProbability;
+            
+                var mutationOperator = new MutationA(mutationProbability);
+            var geneticAlgorithm = new GeneticAlgorithm(pcb, w1, w2, w3, w4, w5);
+            var crossoverOperator = new UniformCrossover(crossoverProbability);
+
+            _logger.Info("----------------------------------------------");
+            _logger.Info("-- Started tournament size investigations --");
+            _logger.Info("----------------------------------------------");
+            var startedPopulation = GeneticAlgorithmUtils.GetStartedPopulation(pcb, populationSize);
+
+            foreach (var size in tournamentSizes)
+            {
+                var outputFilePath = $"{outputFilePathPrefix}-{size.ToString()}{outputFilePathExtension}";
+                File.WriteAllLines(outputFilePath, new[] {ContentHeaderText});
+
+            var selectionOperator = new TournamentSelection(pcb, size, w1, w2, w3, w4, w5);
+                GeneticAlgorithmSolution(outputFilePath, selectionOperator, geneticAlgorithm, crossoverOperator,
+                    mutationOperator, epochsQty, startedPopulation);
+                _logger.Info(
+                    $"Calulated for:  {populationSize.ToString()},{w1.ToString()},{w2.ToString()},{w3.ToString()},{w4.ToString()},{w5.ToString()},{size.ToString()},{crossoverProbability.ToString()},{mutationProbability.ToString()},{epochsQty.ToString()}");
             }
         }
 
